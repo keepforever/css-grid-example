3a82792d38b7b3c1d7f3d0eb65348740abb71dc2
keepforever <briancilenti@gmail.com>
2019-01-26 22:53:08 -0800

    Meh

---
+++ src/comps/MyGrid/MyGrid.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+// import "./styles.css";
+import "./altOne.css";
+
+class MyGrid extends Component {
+  render() {
+    return (
+      <div id="content">
+        <header>
+          header
+        </header>
+        <main>
+          main
+        </main>
+        <aside>
+          aside
+        </aside>
+        <nav>
+          nav
+        </nav>
+        <section>
+          section
+        </section>
+        <footer>
+          footer
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MyGrid;
